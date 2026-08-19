2b378de3296f5730b8861a94923b97e8f4c1b3bf
jave-LGUIDS <jave@lguids.com.ph>
2026-08-19 09:17:06 +0800

    Delete Esperanza-Web-Admin directory

--- Esperanza-Web-Admin/resources/views/components/ui/t.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['fil', 'en'])
-
-<span x-text="$store.lang.current === 'en' ? @js($en) : @js($fil)">{{ $fil }}</span>
--- Esperanza-Web-Admin/resources/views/components/ui/table.blade.php
+++
@@ -1,9 +0,0 @@
-@props(['padded' => false])
-
-<div {{ $attributes->merge(['class' => 'bg-white rounded-2xl border border-slate-100 shadow-card overflow-hidden']) }}>
-    <div class="overflow-x-auto scrollbar-thin">
-        <table class="w-full text-sm {{ $padded ? 'p-5' : '' }}">
-            {{ $slot }}
-        </table>
-    </div>
-</div>
